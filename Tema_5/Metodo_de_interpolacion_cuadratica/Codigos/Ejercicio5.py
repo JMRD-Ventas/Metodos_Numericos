7bab7bd6c8978ef98edc96eb0863fa02b9657e67
Gh-JMZM25 <158504061+Gh-JMZM25@users.noreply.github.com>
2024-06-03 12:28:39 -0600

    Update Ejercicio5.py

--- Tema_5/Metodo_de_interpolacion_cuadratica/Codigos/Ejercicio5.py
+++ Tema_5/Metodo_de_interpolacion_cuadratica/Codigos/Ejercicio5.py
@@ -1,1 +1,24 @@
+#Programa que calcula el porcentaje de inflación en México en 2014
+import numpy as np
 
+# x= año, y= población en millones
+x_points = np.array([2005, 2007, 2021])
+y_points = np.array([3.33, 3.6, 4.65])
+
+# Matriz de Vandermonde para la interpolación cuadrática
+A = np.vander(x_points, 3)
+coefficients = np.linalg.solve(A, y_points)
+
+# Polinomio cuadrático
+def quadratic_polynomial(x, coeffs):
+    return coeffs[0]*x**2 + coeffs[1]*x + coeffs[2]
+
+# Punto a interpolar
+x_to_interpolate = 2014
+y_interpolated = quadratic_polynomial(x_to_interpolate, coefficients)
+
+print(f"El valor interpolado de y para x = {x_to_interpolate} es {y_interpolated}")
+
+# Visualización
+x_range = np.linspace(0, 4, 400)
+y_range = quadratic_polynomial(x_range, coefficients)
